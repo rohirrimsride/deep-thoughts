9a280ee0d7f89ba145990b10f2c7b84ae9b054ce
Strider <dave.barnes23@gmail.com>
2022-09-16 23:05:33 -0500

    worked on adding authentication

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 
 const ThoughtList = ({ thoughts, title }) => {
